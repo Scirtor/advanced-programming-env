02d49985a896d606fec96a3dcb965ed1e5ac18f7
Scirtor <fastboy335@gmail.com>
2025-12-26 16:26:37 +0500

    Have added the 4th and th3 5th tasks

--- Week3/a3task4.py
+++ Week3/a3task4.py
@@ -1,5 +1,48 @@
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+
 def fraction_dividor():
-    return
+    a, b = map(int, input("Enter numerator and denominator of first fraction: ").split())
+    c, d = map(int, input("Enter numerator and denominator of second fraction: ").split())
+
+    numerator = a * d
+    denominator = b * c
+    
+    common_divisor = gcd(numerator, denominator)
+    
+    numerator = numerator // common_divisor
+    denominator = denominator // common_divisor
+    print(f"The result of division is {numerator}/{denominator}")
+    return 
+
+
+
+def is_point_inside_circle(p_x, p_y, center_a, center_b, radius):
+    
+    distance_squared = (p_x - center_a) ** 2 + (p_y - center_b) ** 2
+    return distance_squared < radius ** 2
+
 
 def points_in_circle():
-    return
+    center_a, center_b = map(int, input("Enter center coordinates of the circle: ").split())
+    radius = int(input("Enter radius of the circle: "))
+    count = 0
+    n = int(input("Enter number of points: "))
+    points = []
+    for _ in range(n):
+        x, y = map(int, input("Enter point coordinates (x y): ").split())
+        points.append((x, y))
+        print(points)
+    inside_points = []
+    
+    for point in points:
+        if is_point_inside_circle(point[0], point[1], center_a, center_b, radius):
+            count += 1
+            inside_points.append(point)
+    
+    print(f"Number of points inside circle: {count}")
+    print(f"Points inside: {inside_points}")
+    return count
--- Week3/a3task5.py
+++ Week3/a3task5.py
@@ -1,5 +1,28 @@
 def fraction_subtractor():
+    def gcd(a, b):
+        while b != 0:
+            a, b = b, a % b
+        return a
+    
+    num1, den1 = map(int, input("Enter numerator and denominator of first fraction: ").split())
+    num2, den2 = map(int, input("Enter numerator and denominator of second fraction: ").split())
+
+    result_num = num1 * den2 - num2 * den1
+    result_den = den1 * den2
+
+    divisor = gcd(abs(result_num), abs(result_den))
+    result_num = result_num // divisor
+    result_den = result_den // divisor
+
+    print(f"Result: {result_num}/{result_den}")
+
     return
 
 def all_divisors():
+    num = int(input("Give me the number: "))
+    divisors = []
+    for i in range(1, num + 1):
+        if num % i == 0:
+            divisors.append(i)
+    print(f"All divisors of {num} are: {divisors}")
     return
